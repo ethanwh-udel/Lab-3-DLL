ab5cdcbdf04bea2771ab375a81964e372c1d02f1
HarrisonLittlepage1 <79233141+HarrisonLittlepage1@users.noreply.github.com>
2021-10-13 18:45:30 -0400

    Add files via upload

--- DNode.cpp
+++ DNode.cpp
@@ -2,18 +2,25 @@
  * DNode.cpp
  *
  *  Created on: Oct 13, 2021
- *      Author: ethan
+ *      Author: harrisonlittlepage
  */
 
 #include "DNode.hpp"
+#include "song.hpp"
 #include <iostream>
 #include <stdlib.h>
+using namespace std;
 
-DNode::DNode() {
+DNode::DNode(){
+	song = NULL;
+	next = NULL;
+	prev = NULL;
+}
 
+DNode::DNode(string s, string a, int lenmin, int lensec){
+	song = new song(string s, string a, int lenmin, int lensec);
+	next = NULL;
+	prev = NULL;
 }
 
 
-DNode::DNode(string s, string a, int lenmin, int lensec) {
-
-}
