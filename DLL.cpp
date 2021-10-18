8815f67d20fcac7bb957d0e3492e5f26fd572b2d
HarrisonLittlepage1 <79233141+HarrisonLittlepage1@users.noreply.github.com>
2021-10-17 22:28:34 -0400

    Add files via upload

--- DLL.cpp
+++ DLL.cpp
@@ -1,8 +1,7 @@
 
 #include "DNode.hpp"
-#include "Playlist.hpp"
-#include "DLL.hpp"
 #include "Song.hpp"
+#include "DLL.hpp"
 #include <iostream>
 #include <stdlib.h>
 using namespace std;
@@ -21,187 +20,209 @@ using namespace std;
 	}
 
 	void DLL::push(string t, string a, int m, int s){
-		DNode *n = new DNode (t,a,m,s);
+			DNode *n = new DNode (t,a,m,s);
 
-		if(first == NULL){
-			first = n;
-			last = n;
+			if(first == NULL){
+				first = n;
+				last = n;
 
-		}
-		else{
-			n->prev = last;
-			last->next = n;
-			last = n;
-		}
-		numSongs++;
-	}
-
-	void DLL::printList(void){
-		DNode *live = first;
-
-		while(live != NULL){
-			live->song->printSong();
-			live = live->next;
-		}
-	}
-
-	int DLL::remove(string t){
-		DNode *song = first;
-
-		if(song->song->title == t){
-			cout<<"Removing:";
-			song->song->printSong();
-			song->next->prev = song ->prev;
-			first = song->next;
-			delete song;
-			return 0;
-		}
-
-		while(song->next != NULL){
-			cout<<"Removing:";
-			song->song->printSong();
-			song->prev->next = song->next;
-			song->next->prev = song ->prev;
-			delete song;
-			return 0;
-		}
-
-
-
-		if(last->song->title == t){
-			cout<<"Removing:";
-			pop()->printSong();
-			return 0;
-		}
-
-		cout<<"Remove Failed, "<< t << "not in list.";
-		return -1;
-
-	}
-
-	Song *DLL::pop(){
-		if(numSongs == 1){
-			first = NULL;
-			DNode *temp = last;
-			Song *song = temp->song;
-			last = last->prev;
-			delete temp;
-			last = NULL;
-			return song;
-		}
-		else{
-			DNode *temp = last;
-			Song *song = temp->song;
-			last = last->prev;
-			delete temp;
-			numSongs--;
-			return song;
-		}
-	}
-
-	void DLL::moveUp(string t){
-		if(numSongs == 0){
-			cout<<"List is empty"<<endl;
-		}
-		else{
-			DNode *n = first;
-			while(n != NULL){
-				if(n->song->title == t){
-				 if(n->prev == NULL){
-					 first = n->next;
-					 first->prev = NULL;
-					 last->next = n;
-					 n->prev = last;
-					 last = n;
-					 last->next = NULL;
-					 break;
-				 }
-				 else if(n->next == NULL){
-					 n->prev->prev->next = n;
-					 last = n->prev;
-					 n->prev->next = NULL;
-					 n->next = n->prev;
-					 n->prev = n->next->prev;
-					 n->next->prev = n;
-					 break;
-				 }
-				 else if(n->prev->prev == NULL){
-					 n->prev->next = n->next;
-					 n->next->prev = n->prev;
-					 n->prev->prev = n;
-					 n->next = n->prev;
-					 n->prev = NULL;
-					 first = n;
-					 break;
-				 }
-				 else{
-					 n->prev->prev->next = n;
-					 n->next->prev = n->prev;
-					 n->prev->next = n->next;
-					 n->next = n->prev;
-					 n->prev = n->prev->prev;
-					 n->next->prev = n;
-					 break;
-				 }
 			}
-			n = n->next;
+			else{
+				n->prev = last;
+				last->next = n;
+				last = n;
+			}
+			numSongs++;
+		}
+
+		void DLL::printList(void){
+			DNode *live = first;
+
+			while(live != NULL){
+				live->song->printSong();
+				live = live->next;
 			}
 		}
-	}
 
-	void DLL::moveDown(string t){
-		if(numSongs == 0){
-			cout<<"List is empty"<<endl;
+		int DLL::remove(string t){
+			DNode *song = first;
+
+			if(song->song->title == t){
+				cout<<"Removing:";
+				song->song->printSong();
+				song->next->prev = song ->prev;
+				first = song->next;
+				delete song;
+				return 0;
+			}
+
+			while(song->next != NULL){
+				cout<<"Removing:";
+				song->song->printSong();
+				song->prev->next = song->next;
+				song->next->prev = song ->prev;
+				delete song;
+				return 0;
+			}
+
+
+
+			if(last->song->title == t){
+				cout<<"Removing:";
+				pop()->printSong();
+				return 0;
+			}
+
+			cout<<"Remove Failed, "<< t << "not in list.";
+			return -1;
+
 		}
-		else{
-			DNode *n = first;
-			while(n != NULL){
-				if(n->song->title == t){
-					if(n->prev == NULL){
-						n->next->next->prev = n;
-						first = n->next;
-						first->prev = NULL;
-						n->next = first->next;
-						first->next = n;
-						n->prev = first;
-					}
-					else if(n->next->next == NULL){
-						n->prev->next = n->next;
-						n->next->prev = n->prev;
-						n->next->next = n;
-						n->prev = n->next;
-						last = n;
-						n->next = NULL;
-					}
-					else if(n->next == NULL){
-						 first->prev = n;
-						 n->next = first;
-						 first = n;
-						 last = n->prev;
+
+		Song *DLL::pop(){
+			if(numSongs == 1){
+				first = NULL;
+				DNode *temp = last;
+				Song *song = temp->song;
+				last = last->prev;
+				delete temp;
+				last = NULL;
+				return song;
+			}
+			else{
+				DNode *temp = last;
+				Song *song = temp->song;
+				last = last->prev;
+				delete temp;
+				numSongs--;
+				return song;
+			}
+		}
+
+		void DLL::moveUp(string t){
+			if(numSongs == 0){
+				cout<<"List is empty"<<endl;
+			}
+			else{
+				DNode *n = first;
+				while(n != NULL){
+					if(n->song->title == t){
+					 if(n->prev == NULL){
+						 first = n->next;
+						 first->prev = NULL;
+						 last->next = n;
+						 n->prev = last;
+						 last = n;
 						 last->next = NULL;
+						 break;
+					 }
+					 else if(n->next == NULL){
+						 n->prev->prev->next = n;
+						 last = n->prev;
+						 n->prev->next = NULL;
+						 n->next = n->prev;
+						 n->prev = n->next->prev;
+						 n->next->prev = n;
+						 break;
+					 }
+					 else if(n->prev->prev == NULL){
+						 n->prev->next = n->next;
+						 n->next->prev = n->prev;
+						 n->prev->prev = n;
+						 n->next = n->prev;
 						 n->prev = NULL;
-					}
-					else{
-						n->prev->next = n->next;
-						n->next->prev = n->prev;
-						n->next = n->next->next;
-						n->prev = n->next->prev;
-						n->next->prev->next = n;
-						n->next->prev = n;
-					}
+						 first = n;
+						 break;
+					 }
+					 else{
+						 n->prev->prev->next = n;
+						 n->next->prev = n->prev;
+						 n->prev->next = n->next;
+						 n->next = n->prev;
+						 n->prev = n->prev->prev;
+						 n->next->prev = n;
+						 break;
+					 }
 				}
 				n = n->next;
+				}
 			}
 		}
-	}
 
-	void DLL::makeRandom(){
+		void DLL::moveDown(string t){
+			if(numSongs == 0){
+				cout<<"List is empty"<<endl;
+			}
+			else{
+				DNode *n = first;
+				while(n != NULL){
+					if(n->song->title == t){
+						if(n->prev == NULL){
+							n->next->next->prev = n;
+							first = n->next;
+							first->prev = NULL;
+							n->next = first->next;
+							first->next = n;
+							n->prev = first;
+						}
+						else if(n->next->next == NULL){
+							n->prev->next = n->next;
+							n->next->prev = n->prev;
+							n->next->next = n;
+							n->prev = n->next;
+							last = n;
+							n->next = NULL;
+						}
+						else if(n->next == NULL){
+							 first->prev = n;
+							 n->next = first;
+							 first = n;
+							 last = n->prev;
+							 last->next = NULL;
+							 n->prev = NULL;
+						}
+						else{
+							n->prev->next = n->next;
+							n->next->prev = n->prev;
+							n->next = n->next->next;
+							n->prev = n->next->prev;
+							n->next->prev->next = n;
+							n->next->prev = n;
+						}
+					}
+					n = n->next;
+				}
+			}
+		}
 
-	}
+		void DLL::makeRandom(){
 
 
+		}
 
+		void DLL::listDuration(int *tm, int *ts){
 
+			if(numSongs == 0){
+				*tm = 0;
+				*ts = 0;
+			}
+			else{
+				DNode *n = first;
+				while(n != NULL){
+					*tm += n->song->min;
+					*ts += n->song->sec;
+					n = n->next;
+				}
+			}
 
+		}
+
+		DLL::~DLL(){
+			while(first){
+				cout<<"Deleting: "<< pop()->title<<endl;
+			}
+			first = NULL;
+			last = NULL;
+
+		}
 
 
